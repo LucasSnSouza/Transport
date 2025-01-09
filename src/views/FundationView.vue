609b4e32df1a2f131be462e8a1365ac1cc185e87
LucasSnSouza <lucassantospublico@gmail.com>
2025-01-09 17:35:03 -0300

    fix / fix day

--- src/views/FundationView.vue
+++ src/views/FundationView.vue
@@ -10,105 +10,130 @@
             </div>
 
             <div class="w-full">
-                <h1 class="font-sm">funds</h1>
+                <h1 class="font-sm">bank</h1>
                 <p class="font-sm color-brand-three">{{ companyFounds - form['funds'] }}</p>
             </div>
 
         </div>
 
-        <div class="flex flex-column gap-xlg">
-
-            <div class="flex gap-lg">
-
-                <InputBasic
-                    v-model="form['name']"
+        <div 
+            v-if="!created"
+            class="flex flex-column gap-xlg"
+        >
+            <div class="flex flex-column gap-xlg">
+            
+                <div class="flex gap-lg">
+    
+                    <InputBasic
+                        v-model="form['name']"
+                        class="ghost rounded-sm color-brand-five"
+                        input-class="p-lg font-sm color-brand-two"
+                        placeholder="Company Name"
+                        :value="form['name']"
+                    />
+    
+                    <InputBasic
+                        v-model="form['funds']"
+                        class="ghost rounded-sm color-brand-five flex"
+                        input-class="p-lg font-sm color-brand-two"
+                        type="number"
+                        placeholder="Funds"
+                        :value="form['funds']"
+                    >
+                    </InputBasic>
+    
+                </div>
+    
+                <div class="flex gap-lg">
+    
+                    <InputBasic
+                        v-model="form['cep']"
+                        class="ghost rounded-sm color-brand-five"
+                        input-class="p-lg font-sm color-brand-two"
+                        placeholder="Company Cep"
+                        type="number"
+                        :value="form['cep']"
+                    />
+    
+                    <InputBasic
+                        v-model="form['state']"
+                        :disabled="true"
+                        class="ghost rounded-sm color-brand-five flex"
+                        input-class="p-lg font-sm color-brand-two"
+                        placeholder="State"
+                        :value="form['state']"
+                    />
+    
+                    <InputBasic
+                        v-model="form['city']"
+                        :disabled="true"
+                        class="ghost rounded-sm color-brand-five flex"
+                        input-class="p-lg font-sm color-brand-two"
+                        placeholder="City"
+                        :value="form['city']"
+                    />
+    
+                </div>
+    
+                <div class="flex gap-lg">
+    
+                    <InputBasic
+                        v-model="form['neighborhood']"
+                        :disabled="true"
+                        class="ghost rounded-sm color-brand-five flex"
+                        input-class="p-lg font-sm color-brand-two"
+                        placeholder="Neighborhood"
+                        :value="form['neighborhood']"
+                    />
+                    
+                    <InputBasic
+                        v-model="form['street']"
+                        :disabled="true"
+                        class="ghost rounded-sm color-brand-five flex"
+                        input-class="p-lg font-sm color-brand-two"
+                        placeholder="Street"
+                        :value="form['street']"
+                    />
+    
+                </div>
+    
+                <InputText
+                    v-model="form['description']"
                     class="ghost rounded-sm color-brand-five"
                     input-class="p-lg font-sm color-brand-two"
-                    placeholder="Company Name"
-                    :value="form['name']"
+                    placeholder="Description"
+                    :value="form['description']"
                 />
-
-                <InputBasic
-                    v-model="form['funds']"
-                    class="ghost rounded-sm color-brand-five flex"
-                    input-class="p-lg font-sm color-brand-two"
-                    type="number"
-                    placeholder="Funds"
-                    :value="form['funds']"
+    
+            </div>
+    
+            <div>
+                <ButtonBasic
+                    class="bg-color-brand-two color-brand-one p-lg rounded-sm w-half"
+                    @click="created = true"
                 >
-                </InputBasic>
-
+                    <p>Submit</p>
+                </ButtonBasic>
             </div>
-
-            <div class="flex gap-lg">
-
-                <InputBasic
-                    v-model="form['cep']"
-                    class="ghost rounded-sm color-brand-five"
-                    input-class="p-lg font-sm color-brand-two"
-                    placeholder="Company Cep"
-                    type="number"
-                    :value="form['cep']"
-                />
-
-                <InputBasic
-                    v-model="form['state']"
-                    :disabled="true"
-                    class="ghost rounded-sm color-brand-five flex"
-                    input-class="p-lg font-sm color-brand-two"
-                    placeholder="State"
-                    :value="form['state']"
-                />
-
-                <InputBasic
-                    v-model="form['city']"
-                    :disabled="true"
-                    class="ghost rounded-sm color-brand-five flex"
-                    input-class="p-lg font-sm color-brand-two"
-                    placeholder="City"
-                    :value="form['city']"
-                />
-
-            </div>
-
-            <div class="flex gap-lg">
-
-                <InputBasic
-                    v-model="form['neighborhood']"
-                    :disabled="true"
-                    class="ghost rounded-sm color-brand-five flex"
-                    input-class="p-lg font-sm color-brand-two"
-                    placeholder="Neighborhood"
-                    :value="form['neighborhood']"
-                />
-                
-                <InputBasic
-                    v-model="form['street']"
-                    :disabled="true"
-                    class="ghost rounded-sm color-brand-five flex"
-                    input-class="p-lg font-sm color-brand-two"
-                    placeholder="Street"
-                    :value="form['street']"
-                />
-
-            </div>
-
-            <InputText
-                v-model="form['description']"
-                class="ghost rounded-sm color-brand-five"
-                input-class="p-lg font-sm color-brand-two"
-                placeholder="Description"
-                :value="form['description']"
-            />
-
         </div>
 
-        <div>
+        <div 
+            v-else
+            class="flex flex-column y-center gap-xlg"
+        >
+
+            <div>
+                <h1 class="font-md text-center">Congratulations</h1>
+                <p class="font-sm o-half text-center">Your business created of success and now, you can manager your dreams.</p>
+            </div>
+
             <ButtonBasic
                 class="bg-color-brand-two color-brand-one p-lg rounded-sm w-half"
+                @click="$router.push({ path: '/' })"
             >
-                <p>Submit</p>
+                <p>Go Home</p>
             </ButtonBasic>
+
         </div>
 
     </div>
@@ -128,21 +153,28 @@ export default{
         return{
             form: {
                 funds: 0
-            }
+            },
+            created: false,
         }
     },
     watch: {
         'form.cep': function(value) {
             if(value.length > 7){
-                fetch(`https://viacep.com.br/ws/${value}/json/`)
+                fetch(`https://cep.awesomeapi.com.br/json/${value}`)
                     .then((response) => response.json())
                     .then(data => {
                         this.form = {
                             ...this.form,
-                            state: data?.uf,
-                            neighborhood: data?.bairro,
-                            street: data?.logradouro,
-                            city: data?.localidade,
+                            state: data?.state,
+                            neighborhood: data?.district,
+                            street: data?.address,
+                            city: data?.city,
+                            coordenates: {
+                                latitude: data?.lat,
+                                longitude: data?.lng
+                            },
+                            ddd: data?.ddd,
+                            ibge: data?.city_ibge
                         }
                     })
             }
